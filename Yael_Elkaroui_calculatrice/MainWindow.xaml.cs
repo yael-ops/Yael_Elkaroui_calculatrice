1d131c53384f66052243e7888990bacf7342de7d
yael elkaroui <yael.elkaroui@saintmichelannecy.fr>
2023-09-29 13:48:13 +0200

    V1

--- Yael_Elkaroui_calculatrice/MainWindow.xaml.cs
+++ Yael_Elkaroui_calculatrice/MainWindow.xaml.cs
@@ -109,7 +109,7 @@ namespace Yael_Elkaroui_calculatrice
             N1 = Double.Parse(TB_Resultat.Text);
             operation = '-';
             TB_Resultat.Text = "";
-            //guguyghh
+            //guguyghhbvg
         }
 
         private void BTNfois_Click(object sender, RoutedEventArgs e)
